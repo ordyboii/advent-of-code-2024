ce043eb1c2ccaa1f8489766ca19c3704df67d88a
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-12-02 22:55:00 +0000

    day 2

--- app/two/solve.ts
+++ app/two/solve.ts
@@ -2,14 +2,87 @@ import { readFile } from "fs/promises";
 import { resolve } from "path";
 
 async function main() {
-  // Part 1
-  // 1. TODO
-  // const example = await readFile(resolve(__dirname, "example.txt"), "utf-8");
-  // console.log("File input:", example);
+  const example = await readFile(resolve(__dirname, "input.txt"), "utf-8");
+  const reports = example
+    .split("\n")
+    .map((line) => line.split(" ").map(Number));
+
+  type IncreaseOrDecrease = "increase" | "decrease";
+
+  let safeReports = 0;
+  reports.forEach((report) => {
+    if (checkReport(report)) safeReports++;
+  });
+
+  console.log("Safe reports:", safeReports);
+
   // Part 2
-  // 1. TODO
+
+  let newSafeReports = 0;
+  reports.forEach((report) => {
+    if (checkReport(report)) {
+      newSafeReports += 1;
+    } else {
+      for (let i = 0; i < report.length; i++) {
+        const modifiedReport = report.slice(0, i).concat(report.slice(i + 1));
+        if (checkReport(modifiedReport)) {
+          newSafeReports += 1;
+          break;
+        }
+      }
+    }
+  });
+
+  console.log("New safe reports:", newSafeReports);
+
+  // Common
+
+  function checkReport(report: number[]) {
+    let unsafeReport = false;
+    let previousIncreaseOrDecrease: IncreaseOrDecrease | undefined;
+    let previousNumber: number | undefined;
+
+    for (let i = 0; i < report.length; i++) {
+      const currentNumber = report[i];
+      if (i === 0) {
+        previousNumber = currentNumber;
+        continue;
+      }
+
+      let increaseOrDecrease: IncreaseOrDecrease;
+
+      if (currentNumber > previousNumber!) {
+        increaseOrDecrease = "increase";
+      } else if (currentNumber < previousNumber!) {
+        increaseOrDecrease = "decrease";
+      } else {
+        unsafeReport = true;
+        break;
+      }
+
+      const difference = Math.abs(currentNumber - previousNumber!);
+
+      if (difference < 1 || difference > 3) {
+        unsafeReport = true;
+        break;
+      }
+
+      if (
+        previousIncreaseOrDecrease &&
+        previousIncreaseOrDecrease !== increaseOrDecrease
+      ) {
+        unsafeReport = true;
+        break;
+      }
+
+      previousIncreaseOrDecrease = increaseOrDecrease;
+      previousNumber = currentNumber;
+    }
+
+    return !unsafeReport;
+  }
 }
 
 main().catch((error) => {
-  console.error(`An error occured ${error}`);
+  console.error(`An error occurred: ${error}`);
 });
